d0e251b866ef231a5cf9f988969c45fbe35160d1
Artyom <artyom.zayarny@gmail.com>
2017-09-15 10:52:05 +0300

    Initial project

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Smart TV IPTV</title>
+	<link rel="stylesheet" href="assets/dist/style.css">
+</head>
+
+<body>
+	<section class="hero">
+		<div class="container">
+			<h1 class="hero__title">Smart TV IPTV</h1>
+		</div>
+	</section>
+	<section class="smart-iptv">
+		<div class="container">
+			<h2 class="smart-iptv__subtitle">Smart TV</h2>
+			<p class="smart-iptv__desc">Smart TV – technology, that provides the new capabilities in TV Sets. It could be integrated in TV receiver itself and activated by proprietary firmvare or provided by installing additional prefix. Smart TV devices are equipped with CPU and run with on the fully operational OS. They support the Internet connection and allow to use web-applications, view web-pages in browser, get access to stream video services and VOD services, IPTV and Digital TV programs.</p>
+			<h2 class="smart-iptv__subtitle">IPTV</h2>
+			<p class="smart-iptv__desc">
+				IPTV - is a service of digital TV programs transmission by Internet Protocol from your provider media server. This technology provides high-quality video signal because of fast connection and antijamming features. IPTV is supported by all of Smart TV products.</p>
+		</div>
+	</section>
+
+	<section class="tune">
+		<div class="container">
+			<p class="tune__desc">Most of the modern TV sets from such brands as <b>Phillips</b>, <b>Samsung</b>, <b>Toshiba</b>, <b>LG</b>and <b>Sharp </b>are equipped with Smart TV technology. The manufacturers create the original software to launch Smart TV. To tune all the preferences and full list of available channels we recommend you to order customization from our tech support engineer.</p>
+
+			<p class="tune__desc">Our tech expert will:</p>
+			<ul class="tune__list">
+				<li class="tune__list__item">
+				<span class="tune__list__item__span">Tune all the ethereal, satellite and digital TV channels</span></li>
+				<li class="tune__list__item">
+				<span class="tune__list__item__span">Install applications on your demand: VoD and stream video services, news and weather widgets, social networks, games and messengers</span></li>
+				<li class="tune__list__item">
+				<span class="tune__list__item__span">Set and launch all the spectrum of Smart TV compatible devices: Wi-Fi router, IPTV prefix and Blu-Ray CD player</span></li>
+			</ul>
+		</div>
+	</section>
+
+
+
+</body>
+
+</html>
